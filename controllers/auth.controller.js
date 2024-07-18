25b5c4b5faf07a18cd36a1d5b2920cc4e0526b91
star01234 <burit.srisawan@gmail.com>
2024-07-18 11:25:31 +0700

    Auth api without middleware

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,50 +4,103 @@ const User = db.User;
 const Role = db.Role;
 const jwt = require("jsonwebtoken"); //ต้องลง npm i jsonwebtoken
 const bcrypt = require("bcryptjs"); //ต้องลง npm i bcryptjs
-const {Op} = require("sequelize");
+const { Op } = require("sequelize");
+const { route } = require("../routers/restaurant.router");
 
 //Register a new user
 exports.signup = async (req, res) => {
-    const {username, email, password} = req.body;
-    if(!username || !email || !password) {
-        res.status(400).send({
-            message: "Please provide all required fields"
-        });
-        return;
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400).send({
+      message: "Please provide all required fields",
+    });
+    return;
+  }
+
+  // Prepare user data
+  const newUser = {
+    username: username,
+    email: email,
+    password: bcrypt.hashSync(password, 10), // Use appropriate salt rounds
+  };
+
+  try {
+    // Save user in the database
+    const user = await User.create(newUser);
+
+    if (req.body.roles) {
+      const roles = await Role.findAll({
+        where: {
+          name: { [Op.or]: req.body.roles },
+        },
+      });
+      await user.setRoles(roles);
+      res.send({
+        message: "User registered successfully!",
+      });
+    } else {
+      // Set default role to "user" id=1
+      await user.setRoles([1]);
+      res.send({
+        message: "User registered successfully!",
+      });
     }
+  } catch (error) {
+    res.status(500).send({
+      message:
+        error.message || "An error occurred while registering a new user",
+    });
+  }
+};
 
-    //Prepaer user data
-    const newUser = {
-        username:username,
-        email:email,
-        password:bcrypt.hashSync(password, 9),
-    };
-
-    //Save user in the database
-    await User.create(newUser).then((user) => {
-        if(res.body.role){
-            Role.findAll({
-                where:{
-                    name: {[Op.or]: req.body.role},
-                }
-            }).then((roles)=>{
-                user.setRoles(roles).then(()=>{
-                    res.send({
-                        message: "User registered successfully!"
-                    })
-                })
-            });
-        }else{
-            //set defautl rple to "user" id=1
-            user.setRoles([1]).then(() => {
-                res.send({
-                  message: "User registered successfully!",
-                });
-            });
-        }
-    }).catch((error) => {
-        res.status(500).send({
-            message: error.message || "Something error occured while registering a new user."
+//Signin
+exports.signin = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400).send({
+      message: "Please provide all required fields",
+    });
+    return;
+  }
+  //Select *From User where username = "username";
+  await User.findOne({
+    where: { username: username },
+  })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "User not found" });
+      }
+      const passwordIsValid = bcrypt.compareSync(password, user.password);
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: "Invalid password!",
         });
+      }
+      const token = jwt.sign({ id: user.id }, config.secret, {
+        expiresIn: 86400, // 24 h
+      });
+
+      roles.id;
+      const authorities = [];
+      user.getRoles().then((roles) => {
+        for (let i = 0; i < roles.length; i++) {
+          authorities.push("ROLES_" + roles[i].name.toUpperCase());
+        }
+        res.status(200).send({
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          roles: authorities,
+          accessToken: token,
+        });
+      });
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message:
+          error.message ||
+          "Something error occurred wile registering a new user",
+      });
     });
 };
--- routers/auth.router.js
+++ routers/auth.router.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/auth.controller");
 
 router.post("/signup", authController.signup);
 
-module.exports = router;
+router.post("/signin", authController.signin);
 
+module.exports = router;

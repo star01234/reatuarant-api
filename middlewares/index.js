2f3b6c148f0ead5b0c7bda80d913d868a83c206d
star01234 <burit.srisawan@gmail.com>
2024-07-25 16:47:08 +0700

    1647

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,6 @@
 const verifySignUp = require("./verifySingnUp");
-
+const authJwt = require("./authJwt");
 module.exports = {
   verifySignUp,
+  authJwt,
 };
--- routers/restaurant.router.js
+++ routers/restaurant.router.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const restaurantController = require("../controllers/restaurant.controllers");
+const {authJwt} = require ("../middlewares");
 
 //Create a restaurant
 //POST http://localhost:5000/api/v1/restaurants/
-router.post("/", restaurantController.create);
+router.post("/",[authJwt.verifyToken,authJwt.isModOrAdmin], restaurantController.create);
 
 //Get All restaurant
 router.get("/", restaurantController.getAll);
 
 //get a restaurant by ID
-router.get("/:id",restaurantController.getById);
+router.get("/:id",[authJwt.verifyToken],restaurantController.getById);
 
 //update a restaurant
-router.put("/:id",restaurantController.update);
+router.put("/:id",[authJwt.verifyToken,authJwt.isModOrAdmin],restaurantController.update);
 
 //delete a restaurant
-router.delete("/:id", restaurantController.delete);
+router.delete(
+  "/:id",[authJwt.verifyToken, authJwt.isAdmin],restaurantController.delete
+);
 
 module.exports = router

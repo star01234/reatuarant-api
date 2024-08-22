dda3882cf63a35a8a7b8eed7ec98863ca73bb998
star01234 <burit.srisawan@gmail.com>
2024-08-22 17:10:19 +0700

    17

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,107 +1,85 @@
 const config = require("../config/auth.config");
-const db = require("../models/"); //index
+const db = require("../models/");
 const User = db.User;
 const Role = db.Role;
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { Op } = require("sequelize");
 
-//Register a new user
+// Register a new user
 exports.signup = async (req, res) => {
-  const { username, email, password } = req.body;
-  if (!username || !email || !password) {
-    res.status(400).send({
-      message: "Please provide all required fields!",
-    });
-    return;
-  }
+  try {
+    const { username, email, password, roles } = req.body;
 
-  //Prepare user data
-  const newUser = {
-    username: username,
-    email: email,
-    password: bcrypt.hashSync(password, 8),
-  };
+    if (!username || !email || !password) {
+      return res.status(400).send({ message: "Please provide all required fields!" });
+    }
 
-  //Save user in the database
-  await User.create(newUser)
-    .then((user) => {
-      if (req.body.roles) {
-        Role.findAll({
-          where: {
-            name: { [Op.or]: req.body.roles },
-          },
-        }).then((roles) => {
-          user.setRoles(roles).then(() => {
-            res.send({
-              message: "User registered successfully!",
-            });
-          });
-        });
-      } else {
-        //set default role to "user" id = 1
-        user.setRoles([1]).then(() => {
-          res.send({
-            message: "User registered successfully!",
-          });
-        });
-      }
-    })
-    .catch((error) => {
-      res.status(500).send({
-        message:
-          error.message || "Something error occured while creating the user.",
+    // Prepare user data
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    const newUser = { username, email, password: hashedPassword };
+
+    // Save user in the database
+    const user = await User.create(newUser);
+
+    if (roles) {
+      const rolesData = await Role.findAll({
+        where: { name: { [Op.or]: roles } }
       });
-    });
+      await user.setRoles(rolesData);
+    } else {
+      // Set default role to "user" (id = 1)
+      await user.setRoles([1]);
+    }
+
+    res.send({ message: "User registered successfully!" });
+
+  } catch (error) {
+    res.status(500).send({ message: error.message || "Something went wrong while registering the user." });
+  }
 };
 
-//Signin
+// Sign in
 exports.signin = async (req, res) => {
-  const { username, password } = req.body;
-  if (!username || !password) {
-    res.status(400).send({
-      message: "Please provide all required fields!",
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).send({ message: "Please provide all required fields!" });
+    }
+
+    // Find user by username
+    const user = await User.findOne({ where: { username } });
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found." });
+    }
+
+    // Validate password
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        accessToken: null,
+        message: "Invalid Password!",
+      });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ id: user.id }, config.secret, { expiresIn: 86400 }); // 24 hours
+
+    // Get user roles
+    const roles = await user.getRoles();
+    const authorities = roles.map(role => "ROLES_" + role.name.toUpperCase());
+
+    res.status(200).send({
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      roles: authorities,
+      accessToken: token,
     });
-    return;
+
+  } catch (error) {
+    res.status(500).send({ message: error.message || "Something went wrong while signing in." });
   }
-
-  // SQL : SELECT * FROM User WHERE username = "username"
-  await User.findOne({ where: { username: username } })
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send({
-          message: "User not found.",
-        });
-      }
-      const passwordIsValid = bcrypt.compareSync(password, user.password);
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          accessToken: null,
-          message: "Invalid Password!",
-        });
-      }
-      const token = jwt.sign({ id: user.username }, config.secret, {
-        expiresIn: 86400, // 24 hours // 86400
-      });
-
-      const authorities = [];
-      user.getRoles().then((roles) => {
-        for (let i = 0; i < roles.length; i++) {
-          authorities.push("ROLES_" + roles[i].name.toUpperCase());
-        }
-        res.status(200).send({
-          id: user.username,
-          username: user.username,
-          email: user.email,
-          roles: authorities,
-          accessToken: token,
-        });
-      });
-    })
-    .catch((error) => {
-      res.status(500).send({
-        message:
-          error.message || "Something error occured while creating the user.",
-      });
-    });
 };
